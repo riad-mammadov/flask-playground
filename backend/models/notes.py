0334d0f7f8e8526c1e79f85bc38a2cbf00dbf5b3
Riad <riad_mammadov@yahoo.com>
2025-11-26 19:59:54 +0000

    create Note model + configure DeclarativeBase

---
+++ backend/models/notes.py
@@ -0,0 +1,11 @@
+from db import db
+from sqlalchemy import func
+
+
+class Note(db.Model):
+    __tablename__ = "notes"
+
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(200), nullable=False)
+    content = db.Column(db.Text, nullable=False)
+    created_at = db.Column(db.DateTime(timezone=True), server_default=func.now())
