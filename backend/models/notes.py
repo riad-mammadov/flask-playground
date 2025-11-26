a42cbd5449aa8741359673afa91b54059f6f6aaf
Riad <riad_mammadov@yahoo.com>
2025-11-26 22:09:18 +0000

    drop created_at column

--- backend/models/notes.py
+++ backend/models/notes.py
@@ -8,4 +8,3 @@ class Note(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(200), nullable=False)
     content = db.Column(db.Text, nullable=False)
-    created_at = db.Column(db.DateTime(timezone=True), server_default=func.now())
